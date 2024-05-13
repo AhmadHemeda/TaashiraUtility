100bc080c11965be8ab6048cd80bd5f6331759be
AhmadHemeda <77977659+AhmadHemeda@users.noreply.github.com>
2024-05-13 16:07:33 +0300

    first commit

---
+++ Sources/TaashiraUtility/Network/APIRequestProvider/APIRequestProvider.swift
@@ -0,0 +1,30 @@
+import Foundation
+import Combine
+
+@available(iOS 13.0, *)
+open class APIRequestProvider: APIRequestProviderProtocol {
+    public static let shared = APIRequestProvider()
+    
+    private let session: URLSession
+    
+    private init(session: URLSession = URLSession.shared) {
+        self.session = session
+    }
+    
+    public func makeRequest<T: APIRequestProtocol>(request: T) -> AnyPublisher<T.ResponseType, Error> {
+        var urlRequest = URLRequest(url: request.baseURL.appendingPathComponent(request.path))
+        urlRequest.httpMethod = request.method.rawValue
+        urlRequest.allHTTPHeaderFields = request.headers
+        urlRequest.httpBody = request.body
+        
+        return session.dataTaskPublisher(for: urlRequest)
+            .tryMap { data, response in
+                guard let httpResponse = response as? HTTPURLResponse, (200..<300).contains(httpResponse.statusCode) else {
+                    throw NSError(domain: "NetworkingErrorDomain", code: 0, userInfo: nil)
+                }
+                return data
+            }
+            .decode(type: T.ResponseType.self, decoder: JSONDecoder())
+            .eraseToAnyPublisher()
+    }
+}
---
+++ Sources/TaashiraUtility/Network/APIRequestProvider/APIRequestProviderProtocol.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  
+//
+//  Created by Ahmad Hemeda on 13/05/2024.
+//
+
+import Foundation
