15e3caaa2ad0c5ed0cbadfffe99012392d5dc8ed
AhmadHemeda <77977659+AhmadHemeda@users.noreply.github.com>
2024-05-28 14:37:01 +0300

    TaashiraUtility modify APIRequestProvider.swift to map the error

--- Sources/TaashiraUtility/Network/APIRequestProvider/APIRequestProvider.swift
+++ Sources/TaashiraUtility/Network/APIRequestProvider/APIRequestProvider.swift
@@ -16,22 +16,23 @@ open class APIRequestProvider: APIRequestProviderProtocol {
         urlRequest.httpMethod = request.method.rawValue
         urlRequest.allHTTPHeaderFields = request.headers
         urlRequest.httpBody = request.body
-        
+
         return session.dataTaskPublisher(for: urlRequest)
             .tryMap { data, response in
-                print("Raw Response Data: \(String(data: data, encoding: .utf8) ?? "Unable to decode data")")
-                guard let httpResponse = response as? HTTPURLResponse else {
+                guard let httpResponse = response as? HTTPURLResponse, (200..<300).contains(httpResponse.statusCode) else {
                     throw NSError(domain: "NetworkingErrorDomain", code: 0, userInfo: nil)
                 }
-                
-                if (200..<300).contains(httpResponse.statusCode) {
-                    return data
-                } else {
-                    let errorResponse = try JSONDecoder().decode(APIErrorResponse.self, from: data)
-                    throw APIError.serverError(errorResponse)
-                }
+                print("Raw Response Data: \(String(data: data, encoding: .utf8) ?? "Unable to decode data")")
+                return data
             }
             .decode(type: T.ResponseType.self, decoder: JSONDecoder())
+            .mapError { error -> Error in
+                if let apiError = error as? APIError {
+                    return apiError
+                } else {
+                    return NSError(domain: "NetworkingErrorDomain", code: 0, userInfo: nil)
+                }
+            }
             .eraseToAnyPublisher()
     }
 }
--- Sources/TaashiraUtility/Network/Errors/APIError.swift
+++ Sources/TaashiraUtility/Network/Errors/APIError.swift
@@ -1,10 +1,4 @@
-import Foundation
-
-public struct APIErrorResponse: Decodable {
-    public let message: String
-    public let errors: [String: [String]]
-}
-
-public enum APIError: Error {
-    case serverError(APIErrorResponse)
+public struct APIError: Error {
+    let message: String
+    let errors: [String: [String]]?
 }
