f6b0162fb5d1eca88c0c9233796f523d576a8898
AhmadHemeda <77977659+AhmadHemeda@users.noreply.github.com>
2024-05-28 15:50:44 +0300

    Change the decoder for the response and the error response

--- Sources/TaashiraUtility/Network/APIRequestProvider/APIRequestProvider.swift
+++ Sources/TaashiraUtility/Network/APIRequestProvider/APIRequestProvider.swift
@@ -19,19 +19,14 @@ open class APIRequestProvider: APIRequestProviderProtocol {
 
         return session.dataTaskPublisher(for: urlRequest)
             .tryMap { data, response in
-                guard let httpResponse = response as? HTTPURLResponse, (200..<300).contains(httpResponse.statusCode) else {
-                    throw NSError(domain: "NetworkingErrorDomain", code: 0, userInfo: nil)
+                let decoder = JSONDecoder()
+                if let httpResponse = try? decoder.decode(
+                    T.ResponseType.self, from: data
+                ) {
+                    return httpResponse
                 }
-                print("Raw Response Data: \(String(data: data, encoding: .utf8) ?? "Unable to decode data")")
-                return data
-            }
-            .decode(type: T.ResponseType.self, decoder: JSONDecoder())
-            .mapError { error -> Error in
-                if let apiError = error as? APIError {
-                    return apiError
-                } else {
-                    return NSError(domain: "NetworkingErrorDomain", code: 0, userInfo: nil)
-                }
+                
+                throw try decoder.decode(APIError.self, from: data)
             }
             .eraseToAnyPublisher()
     }
--- Sources/TaashiraUtility/Network/Errors/APIError.swift
+++ Sources/TaashiraUtility/Network/Errors/APIError.swift
@@ -1,4 +1,4 @@
-public struct APIError: Error {
+public struct APIError: Error, Decodable {
     public let message: String
     public let errors: [String: [String]]?
 }
