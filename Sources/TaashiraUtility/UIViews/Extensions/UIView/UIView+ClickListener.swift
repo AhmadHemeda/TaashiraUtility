f3d75ca335f020e6646b45090c37978b24973485
Shaimaa147 <shaimaa.mohammed773@gmail.com>
2024-05-28 12:13:00 +0300

    edit UIViewClickListener

--- Sources/TaashiraUtility/UIViews/Extensions/UIView/UIView+ClickListener.swift
+++ Sources/TaashiraUtility/UIViews/Extensions/UIView/UIView+ClickListener.swift
@@ -9,19 +9,19 @@ import Foundation
 import UIKit
 
 extension UIView {
-    func setOnClickListener(action :@escaping () -> Void){
+    public func setOnClickListener(action :@escaping () -> Void){
         let tapRecogniser = ClickListener(target: self, action: #selector(onViewClicked(sender:)))
         tapRecogniser.onClick = action
         self.addGestureRecognizer(tapRecogniser)
     }
      
-    @objc func onViewClicked(sender: ClickListener) {
+    @objc public func onViewClicked(sender: ClickListener) {
         if let onClick = sender.onClick {
             onClick()
         }
     }
 }
 
-class ClickListener: UITapGestureRecognizer {
+public class ClickListener: UITapGestureRecognizer {
    var onClick : (() -> Void)? = nil
   }
